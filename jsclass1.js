8b287a4617bea6201cc3ee14f554ec9fe32e0755
Jannatulweb <jumalsaif@gmail.com>
2022-09-19 15:56:56 +0600

    changes added

---
+++ jsclass1.js
@@ -0,0 +1,57 @@
+var x=5 //if we write var then we have to write every time
+var y=7
+var z=x+y
+document.write(z)
+console.log(z)
+
+document.write("<br>")
+document.write("<br>")
+
+let m,n,o //if we write let then we have to write only 1time
+m=3
+n=3
+o=m+n
+console.log(o)
+document.write(o)
+
+document.write("<br>")
+document.write("<br>")
+
+p=2
+q=6//here's also i write let only single time 
+let r=p+q
+document.write(r)
+console.log(r)
+
+document.write("<br>")
+document.write("<br>")
+a=2
+b=1
+c=3
+d=(a+b)*c
+document.write(d)
+console.log(d)
+document.write("<br>")
+document.write("<br>")
+
+let X,Y
+X=2
+Y=3
+document.write(X+Y)
+console.log(X+Y)
+
+document.write("<br>")
+document.write("<br>")
+
+let firstName='Jannatul'
+let lastName='Nayeem'
+document.write(firstName+" "+lastName)
+console.log(firstName+" "+lastName)
+
+document.write("<br>")
+document.write("<br>")
+let j,k;
+j=2+1;
+k=j*10;
+document.write(k)
+console.log(k)
